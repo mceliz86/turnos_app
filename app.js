3e66bddee5ca23b2998ebb249b94d3b36fa32ee6
Matias Ojeda <matias_goc@hotmail.com>
2018-11-03 03:47:11 +0000

    add mlab db

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ var express			= require("express"),
 	//var	seedDB = require("./seeds");
 	
 //APP CONFIG	
-mongoose.connect("mongodb://localhost:27017/turnos_app", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost:27017/turnos_app", { useNewUrlParser: true });
+mongoose.connect("mongodb://matias:password1@ds151293.mlab.com:51293/turnos_app", { useNewUrlParser: true });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
